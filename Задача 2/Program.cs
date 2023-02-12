859b6f2486641490cf5ba39227cf7929fd4a8e9f
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-12 12:59:39 +0300

    Добавил решение Задачи №2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (a > b) {
+            Console.WriteLine($"a (= {a}) > b (= {b})");
+        }
+        else {
+            Console.WriteLine($"b (= {b}) > a (= {a})");
+        }
+    }
+}
