8030488c83c151add06513f91b1c87c013abff25
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-12 13:10:01 +0300

    Добавил решение Задачи №6

--- Задача 6/Program.cs
+++ Задача 6/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        
+        Console.Clear();
+
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number%2 == 0) { Console.WriteLine($"Число {number} является чётным");}
+        if (number%2 != 0) { Console.WriteLine($"Число {number} является нечётным");}
     }
 }
