505175af9c45284a0fb237582ee48edd76e83d08
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-12 13:18:56 +0300

    Добавил решение Задачи №8

--- Задача 8/Program.cs
+++ Задача 8/Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
+        Console.Clear();
+
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"{number} ->");
         
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i%2 == 0) { Console.Write($" {i}");}
+            }    
+        }
+        if (number < 0) {
+            for (int i = -1; i >= number; i--) {
+                if (i%2 == 0) { Console.Write($" {i}");}
+            }
+        }
     }
 }
